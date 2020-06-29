db42a4ff007dc656c5eb1d5926bc9f70786e32ae
samanabbasi71 <samyar1992@gmail.com>
2020-06-29 18:29:53 +0430

    name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Alert from '../components/Alert'
 export default function Home() {
     return (
         <React.Fragment>
-            <Alert title="این پروژه صرفا جهت ارایه در رزومه می باشد..."/>
+            <Alert title="این پروژه نمونه کار آقای سامان عباسی جهت ارایه در رزومه می باشد..."/>
             <Hotels />
         </React.Fragment>
     )
